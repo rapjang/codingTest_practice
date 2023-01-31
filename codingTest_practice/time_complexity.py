4345183610cbd37bce192e69d419da5d61f2b8c5
DESKTOP-L657DDQ\User <rapjang@naver.com>
2023-01-31 15:58:06 +0900

    computing test2

--- codingTest_practice/time_complexity.py
+++ codingTest_practice/time_complexity.py
@@ -5,3 +5,19 @@ for i in range(1,101):
     if i == findNumber:
         print(i)
         break
+
+
+# computing count = N
+N = 100000
+cnt = 1
+for i in range(N):
+    print("연산횟수 ", str(cnt))
+    cnt += 1
+    
+# computing count = N*N
+N = 100000
+cnt = 1
+for i in range(N):
+    for j in range(N):
+        print("연산횟수 ", str(cnt))
+        cnt += 1
